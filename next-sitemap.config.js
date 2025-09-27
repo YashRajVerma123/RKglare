614589a7b755c7dceea882616df463b75c390304
Yash Raj Verma <101271631+YashRajVerma123@users.noreply.github.com>
2025-09-27 10:33:03 +0000

    Revert "1st"
    This reverts commit d6b9c264d8c1dd9f84f3ff49382080de5d7e64aa.

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://theglare.netlify.app", // 👈 Your live site URL
-    generateRobotsTxt: true, // (optional but recommended)
-      sitemapSize: 7000,       // (optional, splits large sitemaps)
-      }; */
