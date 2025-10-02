f40833a8cf7ec98e1a8b5ec28d8fa801daf7e866
It's Glint <itsglintop@gmail.com>
2025-10-02 11:24:01 +0000

    but like next-pwa is neccessarry na for that chrome add to home screen f

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,34 @@
-
 /** @type {import('next').NextConfig} */
+import withPWA from "next-pwa";
+
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co' },
-      { protocol: 'https', hostname: 'picsum.photos' },
-      { protocol: 'https', hostname: 'tinyurl.com' },
-      { protocol: 'https', hostname: 'media.gettyimages.com' },
-      { protocol: 'https', hostname: 'static01.nyt.com' },
-      { protocol: 'https', hostname: 'www.hindustantimes.com' },
-      { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' },
-      { protocol: 'https', hostname: 'i.ibb.co' },
-      { protocol: 'https', hostname: 'source.unsplash.com' },
+      { protocol: "https", hostname: "placehold.co" },
+      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "tinyurl.com" },
+      { protocol: "https", hostname: "media.gettyimages.com" },
+      { protocol: "https", hostname: "static01.nyt.com" },
+      { protocol: "https", hostname: "www.hindustantimes.com" },
+      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
+      { protocol: "https", hostname: "i.ibb.co" },
+      { protocol: "https", hostname: "source.unsplash.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" }
     ],
   },
   serverActions: {
-    bodySizeLimit: '4mb',
+    bodySizeLimit: "4mb",
   },
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: isDev,
+});
+
+export default pwaConfig(nextConfig);
